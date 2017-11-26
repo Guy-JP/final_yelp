c7939ea319770ebb52221174c26acc74687affdf
guy-jp <guy.phipps123@gmail.com>
2017-11-26 16:39:12 +0000

    Gems

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,3 @@
-config.assets.initialize_on_precompile = false
-
 require_relative 'boot'
 
 require 'rails/all'
@@ -10,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Yelpdemo
   class Application < Rails::Application
+ config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
