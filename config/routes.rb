e92108f7f43109afa9e7c6db91c35744feddfdab
guy-jp <guy.phipps123@gmail.com>
2017-11-29 15:01:58 +0000

    Contact Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	resources :restaurants
+get '/' => 'restaurants#index'
 get '/restaurants' => 'restaurants#index'
 get '/about' => 'pages#about'
+get '/contact' => 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
