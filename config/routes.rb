f7a7e58dfd9c9ba2ccb9951991cbf552159c7d56
guy-jp <guy.phipps123@gmail.com>
2017-11-29 15:16:29 +0000

    Edit Restaurant Route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	resources :restaurants
 get '/' => 'restaurants#index'
 get '/restaurants' => 'restaurants#index'
+get '/restaurants/edit' => 'restaurants#edit'
 get '/about' => 'pages#about'
 get '/contact' => 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
